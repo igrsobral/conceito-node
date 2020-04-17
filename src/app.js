aa6a2aacd550d7bdd16459c40b792a31b4d3fbd7
igorWebMASTER <igor_1917@hotmail.com>
2020-04-16 23:54:22 -0300

    aplicação node

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -12,14 +12,34 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  return response.json(respositories);
+  
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const  { title, url, techs } = request.body;
+
+  const respository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  if(!repository){
+    return response.status(400).send();
+  }
+
+  repositories.push(respository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+ 
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -28,6 +48,18 @@ app.delete("/repositories/:id", (request, response) => {
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository){
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
+
 });
 
 module.exports = app;
